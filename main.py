2c6620fef9c342891990968a8ff1e6bad5a25e0c
kaivio <kaivio@github.com>
2022-03-31 01:34:46 +0800

    添加日语五十音

--- main.py
+++ main.py
@@ -21,20 +21,23 @@ def colse():
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v',action='count')
-    parser.add_argument('-f',action='store_true')
-    parser.add_argument('-z',action='store_true')
+    parser.add_argument('-v',action='count',help="精准匹配一个单词，-vv 附带扩展内容，-vvv 附带调试信息")
+    parser.add_argument('-f',action='store_true',help="模糊查找单词")
+    parser.add_argument('-z',action='store_true',help="使用中文查找")
+    parser.add_argument('-j',action='count', help='打印日语五十音，-jj 附带浊音 -jjj 附带拗音 如后跟参数，假名/罗马字互转')
     parser.add_argument('--tool',action='store_true')
-    parser.add_argument('key',default='')
+    parser.add_argument('key',nargs='?',default='', help="主参数")
 
     opts,args = parser.parse_known_args()
     k = opts.key
-    if True:
-        pass
+    if opts.j:
+        show_gojyuonzu(opts.j)
+        return
+
     con = connect()
     cur = con.cursor()
     if opts.tool:
-        pass
+        return
 
     elif opts.z:
         sql.find_by_zh(cur, f'*{k}*')
@@ -110,6 +113,167 @@ def find(k):
 
     return rows
 
+def show_gojyuonzu(n):
+    for g in gojyuonzu.strip().split('---'):
+        if n < 0:
+            return
+        for r in g.strip().split('\n\n'):
+            l1,l2 = '  ', '  '
+            for d in r.strip().split('\n'):
+                h,k,i,o = d.split()
+                h=h == '.' and '　' or h
+                k=k == '.' and '　' or k
+                i=i == '.' and ' '  or i
+                o=o == '.' and ' '  or o
+                l1 += f' \033[34m{i:>3}\033[0m {k:2}'
+                l2 += f' {h:>2} \033[35m{o:3}\033[0m'
+
+            print(l1)
+            print(l2)
+        n -= 1
+
+gojyuonzu = '''
+あ ア a .
+い イ i .
+う ウ u .
+え エ e .
+お オ o .
+
+か カ ka .
+き キ ki .
+く ク ku .
+け ケ ke .
+こ コ ko .
+
+さ サ sa .
+し シ shi si
+す ス su .
+せ セ se .
+そ ソ so .
+
+た タ ta .
+ち チ chi ti
+つ ツ tsu tu
+て テ te .
+と ト to .
+
+な ナ na .
+に ニ ni .
+ぬ ヌ nu .
+ね ネ ne .
+の ノ no .
+
+は ハ ha .
+ひ ヒ hi .
+ふ フ fu hu
+へ ヘ he .
+ほ ホ ho .
+
+ま マ ma .
+み ミ mi .
+む ム mu .
+め メ me .
+も モ mo .
+
+や ヤ ya .
+.  .  .  .
+ゆ ユ yu .
+.  .  .  .
+よ ヨ yo .
+
+ら ラ ra .
+り リ ri .
+る ル ru .
+れ レ re .
+ろ ロ ro .
+
+わ ワ wa .
+ゐ ヰ wi .
+.  .  .  .
+ゑ ヱ we .
+を ヲ wo .
+---
+ん ン n .
+---
+が ガ ga .
+ぎ ギ gi .
+ぐ グ gu .
+げ ゲ ge .
+ご ゴ go .
+
+ざ ザ za .
+じ ジ ji zi
+ず ズ zu .
+ぜ ゼ ze .
+ぞ ゾ zo .
+
+だ ダ da .
+ぢ ヂ zi .
+づ ヅ zu .
+で デ de .
+ど ド do .
+
+ば バ ba .
+び ビ bi .
+ぶ ブ bu .
+べ ベ be .
+ぼ ボ bo .
+
+ぱ パ pa .
+ぴ ピ pi .
+ぷ プ pu .
+ぺ ペ pe .
+ぽ ポ po .
+---
+きゃ キャ kya .
+きゅ キュ kyu .
+きょ キョ kyo .
+
+しゃ シャ sha sya
+しゅ シュ shu syu
+しょ ショ sho syo
+
+ちゃ チャ cha tya
+ちゅ チュ chu tyu
+ちょ チョ cho tyo
+
+にゃ ニャ nya .
+にゅ ニュ nyu .
+にょ ニョ nyo .
+
+ひゃ ヒャ hya .
+ひゅ ヒュ hyu .
+ひょ ヒョ hyo .
+
+みゃ ミャ mya .
+みゅ ミュ myu .
+みょ ミョ myo .
+
+りゃ リャ rya .
+りゅ リュ ryu .
+りょ リョ ryo .
+
+ぎゃ ギャ gya .
+ぎゅ ギュ gyu .
+ぎょ ギョ gyo .
+
+じゃ ジャ ja zya
+じゅ ジュ ju zyu
+じょ ジョ jo zyo
+
+ぢゃ ヂャ ja zya
+ぢゅ ヂュ ju zyu
+ぢょ ヂョ jo zyo
+
+びゃ ビャ bya .
+びゅ ビュ byu .
+びょ ビョ byo .
+
+ぴゃ ピャ pya .
+ぴゅ ピュ pyu .
+ぴょ ピョ pyo .
+
+'''
 
 if  __name__ == "__main__":
     try:
